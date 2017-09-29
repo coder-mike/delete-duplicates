078614bf7a61ea0a485136ed44bdcd2565620b77
Michael Hunter <elec.mike@gmail.com>
2017-09-30 09:29:10 +1000

    Removed absolute paths from cache file

--- test/delete-files.js
+++ test/delete-files.js
@@ -21,7 +21,7 @@ describe('delete-files', function() {
           "hash": "0cc175b9c0f1b6a831c399e269772661",
           "mtimeMs": 1506644493902.2776,
           "path": normalizePath(path.resolve(__dirname + "/example/target/source/a.txt")),
-          "relativePath": "./test/example/target/source/a.txt",
+          "relativePath": "a.txt",
           "size": 1
         },
         "duplicates": [
@@ -34,7 +34,7 @@ describe('delete-files', function() {
           "hash": "92eb5ffee6ae2fec3ad71c777531578f",
           "mtimeMs": 1506644502936.1265,
           "path": normalizePath(path.resolve(__dirname + "/example/target/source/sub-folder/b.txt")),
-          "relativePath": "./test/example/target/source/sub-folder/b.txt",
+          "relativePath": "sub-folder/b.txt",
           "size": 1
         },
         "duplicates": [
